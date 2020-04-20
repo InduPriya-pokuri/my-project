578cf138f682c5be9009873ed1bf5b49dc7bbb00
InduPriya-pokuri <pokuriindupriya36@gmail.com>
2020-04-20 17:29:41 +0530

    Updates

---
+++ precache-manifest.5bca4cd6f9b483e3b4c137bc8f4800de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76fba68661021b076d8121564116e4ee",
+    "url": "/my-project/index.html"
+  },
+  {
+    "revision": "ed6b229f15d7ab95786a",
+    "url": "/my-project/static/css/main.430e0980.chunk.css"
+  },
+  {
+    "revision": "79f881cc7e990552b24e",
+    "url": "/my-project/static/js/2.10ed331f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-project/static/js/2.10ed331f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed6b229f15d7ab95786a",
+    "url": "/my-project/static/js/main.9886136c.chunk.js"
+  },
+  {
+    "revision": "e8add440e2a1887f939d",
+    "url": "/my-project/static/js/runtime-main.0df3203f.js"
+  }
+]);
